99834b63c9751b7e423d9ce861f7e3cad0a5d34d
Sean Sundberg <seansund@us.ibm.com>
2020-12-15 19:28:05 -0600

    Adds icr pull secret contents to global pull secret

---
+++ scripts/global-pull-secret.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+CLUSTER_TYPE="$1"
+
+if [[ ! "${CLUSTER_TYPE}" =~ ocp4 ]]; then
+  echo "The cluster is not an OpenShift 4.x cluster. Skipping global pull secret"
+  exit 0
+fi
+
+if [[ -z "${TMP_DIR}" ]]; then
+  TMP_DIR="${PWD}/tmp"
+fi
+mkdir -p "${TMP_DIR}"
+
+GLOBAL_FILE="${TMP_DIR}/global.json"
+ICR_FILE="${TMP_DIR}/icr.json"
+RESULT_FILE="${TMP_DIR}/config.json"
+
+echo "Getting current global pull secret"
+oc get secret pull-secret -n openshift-config -o jsonpath='{ .data.\.dockerconfigjson }' | base64 --decode > "${GLOBAL_FILE}"
+
+if grep -q "icr.io" "${GLOBAL_FILE}"; then
+  echo "The global pull secret already contains the values for icr.io. Nothing to do"
+  exit 0
+fi
+
+echo "Getting icr pull secret"
+oc get secret all-icr-io -n default -o jsonpath='{ .data.\.dockerconfigjson }' | base64 --decode > "${ICR_FILE}"
+
+echo "Merging pull secrets"
+jq -s '.[0] * .[1]' "${GLOBAL_FILE}" "${ICR_FILE}" > "${RESULT_FILE}"
+
+echo "Updating global pull secret"
+oc set data secret/pull-secret -n openshift-config --from-file=".dockerconfigjson=${RESULT_FILE}"
