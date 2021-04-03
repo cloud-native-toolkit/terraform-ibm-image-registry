f4e8b44fb299518907d4d6b8364031e219aa5efc
Sean Sundberg <seansund@us.ibm.com>
2021-04-03 18:08:39 -0500

    Updates cluster module for test
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-cluster.tf
+++ test/stages/stage1-cluster.tf
@@ -1,18 +1,11 @@
 module "dev_cluster" {
-  source = "github.com/ibm-garage-cloud/terraform-ibm-container-platform.git"
+  source = "github.com/cloud-native-toolkit/terraform-ibm-ocp-vpc.git"
 
-  resource_group_name     = var.resource_group_name
+  resource_group_name     = module.resource_group.name
   cluster_name            = var.cluster_name
   cluster_region          = var.region
-  cluster_type            = substr(var.cluster_type, 0, 3) == "iks" ? "kubernetes" : var.cluster_type
+  ocp_version             = "4.6"
   cluster_exists          = true
   ibmcloud_api_key        = var.ibmcloud_api_key
   name_prefix             = var.name_prefix
-  is_vpc                  = var.vpc_cluster
-  private_vlan_id         = ""
-  public_vlan_id          = ""
-  vlan_datacenter         = ""
-  cluster_machine_type    = ""
-  cluster_worker_count    = 3
-  cluster_hardware        = ""
 }
