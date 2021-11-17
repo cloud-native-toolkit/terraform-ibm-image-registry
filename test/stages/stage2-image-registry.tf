bc7042b02a0bd577911b9e403c7031f15fc1f60e
camorae@googlemail.com <camorae@googlemail.com>
2021-11-17 22:39:30 +0100

    fix variable naming causing ibm-garage-iteration-zero install to fail. Terraform was looking for cluster_region, which was renamed to region. This fix sets the variable to the old name.

--- test/stages/stage2-image-registry.tf
+++ test/stages/stage2-image-registry.tf
@@ -2,7 +2,7 @@ module "dev_tools_mymodule" {
   source = "./module"
 
   resource_group_name = module.resource_group.name
-  region              = var.region
+  cluster_region      = var.region
   config_file_path    = module.dev_cluster.config_file_path
   ibmcloud_api_key    = var.ibmcloud_api_key
   cluster_namespace   = module.dev_capture_tools_state.namespace
