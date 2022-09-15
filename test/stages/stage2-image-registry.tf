8d56c75aaf4c7f43d58fd709c2a7dce9825171f2
balasgit <bala@us.ibm.com>
2022-09-15 17:56:27 -0500

    Fix compile error

--- test/stages/stage2-image-registry.tf
+++ test/stages/stage2-image-registry.tf
@@ -6,5 +6,9 @@ module "dev_tools_mymodule" {
   config_file_path    = module.dev_cluster.config_file_path
   ibmcloud_api_key    = var.ibmcloud_api_key
   cluster_namespace   = module.dev_tools_namespace.name
-  registry_namespace   = dev_tools_namespace.name
+  registry_namespace   = local.name-random
+}
+
+locals {
+  name-random = "${local.name_prefix}-${var.namespace}"
 }
