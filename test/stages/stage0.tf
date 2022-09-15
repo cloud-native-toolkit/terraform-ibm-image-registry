822b52730f32b75ab382e9ac45095d591dc9f7d6
balasgit <bala@us.ibm.com>
2022-09-15 18:03:26 -0500

    Fix length size

--- test/stages/stage0.tf
+++ test/stages/stage0.tf
@@ -2,11 +2,11 @@ terraform {
 }
 
 locals {
-  name_prefix = "ee-${random_string.name-prefix.result}"
+  name_prefix = "${random_string.name-prefix.result}"
 }
 
 resource "random_string" "name-prefix" {
-  length           = 5
+  length           = 2
   special          = false
   upper = false
   numeric = false
