1997294b3297a65825052adc2deaa2486f45ca7f
balasgit <bala@us.ibm.com>
2022-09-15 17:55:53 -0500

    fix compile error

--- test/stages/stage1-namespace.tf
+++ test/stages/stage1-namespace.tf
@@ -2,10 +2,10 @@ module "dev_tools_namespace" {
   source = "github.com/cloud-native-toolkit/terraform-k8s-namespace.git"
 
   cluster_config_file_path = module.dev_cluster.config_file_path
-  #name                     = var.namespace
-  name                  = local.name-random
+  name                     = var.namespace
+  #name                  = local.name-random
 }
 
-locals {
-  name-random = "${local.name_prefix}-${var.namespace}"
-}
+# locals {
+#   name-random = "${local.name_prefix}-${var.namespace}"
+# }
