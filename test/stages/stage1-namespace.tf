58bbc8042d0fba8420bf240d78186c989cd53aa7
balasgit <bala@us.ibm.com>
2022-09-15 13:40:37 -0500

    Fix Compile Error

--- test/stages/stage1-namespace.tf
+++ test/stages/stage1-namespace.tf
@@ -4,3 +4,10 @@ module "dev_tools_namespace" {
   cluster_config_file_path = module.dev_cluster.config_file_path
   name                     = var.namespace
 }
+
+resource "random_string" "suffix" {
+  length           = 16
+  special          = true
+  override_special = "/@*$"
+  # Only Alpha numeric - dont pass special $ 
+}
